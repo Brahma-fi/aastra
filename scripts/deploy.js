89c8ad97a189fcdaea552db3df87efcd250a2c85
Bapireddy <bapi@hooklabs.io>
2021-09-15 16:09:44 +0000

    added governance address

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,13 +6,12 @@ const etherscan_verify = true;
 
 const POOL_ADDRESS = "0x2BFD0C3169A8C0Cd7B814D38533A0645368F0D80";
 const STRATEGY_MANAGER_ADDRESS = "0x0405d9d1443DFB051D5e8E231e41C911Dc8393a4";
+const GOVERNANCE_ADDRESS = "0x140713bbD82113e104C3a45661134F9764807922";
 
 async function main() {
 
   const Factory = await hre.ethers.getContractFactory("Factory");
-  const factory = await Factory.deploy();
-
-  const accounts = await hre.ethers.getSigners();
+  const factory = await Factory.deploy(GOVERNANCE_ADDRESS);
 
   await factory.deployed();
 
@@ -38,7 +37,7 @@ async function main() {
   if (etherscan_verify) {
     await hre.run("verify:verify", {
         address: factory.address,
-        constructorArguments: [],
+        constructorArguments: [GOVERNANCE_ADDRESS],
     });
 
     await hre.run("verify:verify", {
