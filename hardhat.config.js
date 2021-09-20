1d5ec5bf514f92df3a138f1b8ca78dcd98d5943b
pradeep-selva <pradeepselvaraju@gmail.com>
2021-09-21 00:38:19 +0530

    deploy scipt changes

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,17 +1,27 @@
 require("@nomiclabs/hardhat-waffle");
-require('hardhat-contract-sizer');
+require("hardhat-contract-sizer");
 require("@nomiclabs/hardhat-etherscan");
 require("solidity-coverage");
-
 
 module.exports = {
   solidity: {
     version: "0.7.5",
     settings: {
-        optimizer: {
-            enabled: true,
-            runs: 50
-        }
+      optimizer: {
+        enabled: true,
+        runs: 50
+      }
     }
   },
+  networks: {
+    customRPC: {
+      url: "https://rpc.tenderly.co/fork/50568bad-900c-402a-8d81-209f3da8c19c"
+    },
+    mainnetFork: {
+      url: "http://13.234.43.171:8545/",
+      accounts: [
+        "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
+      ]
+    }
+  }
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,14 +1,39 @@
 const hre = require("hardhat");
-const fs = require('fs');
 
-const etherscan_verify = true;
+const etherscan_verify = false;
+const tenderly_fork = false;
 
+const tenderlyConfig = {
+  accountId: "bapireddy_1",
+  projectId: "project",
+  forkId: "50568bad-900c-402a-8d81-209f3da8c19c"
+};
 
-const POOL_ADDRESS = "0x2BFD0C3169A8C0Cd7B814D38533A0645368F0D80";
-const STRATEGY_MANAGER_ADDRESS = "0x0405d9d1443DFB051D5e8E231e41C911Dc8393a4";
+const POOL_ADDRESS = "0x8ad599c3a0ff1de082011efddc58f1908eb6e6d8";
+const STRATEGY_MANAGER_ADDRESS = "0x140713bbD82113e104C3a45661134F9764807922";
 const GOVERNANCE_ADDRESS = "0x140713bbD82113e104C3a45661134F9764807922";
 
 async function main() {
+  const signers = await hre.ethers.getSigners();
+  const signerAddr = signers[0].address;
+  console.log(signerAddr);
+
+  // if (tenderly_fork) {
+  //   await fetch(
+  //     `https://api.tenderly.co/api/v1/account/${tenderlyConfig.accountId}/project/${tenderlyConfig.projectId}/fork/${tenderlyConfig.forkId}/balance`,
+  //     {
+  //       method: "POST",
+  //       headers: {
+  //         Accept: "application/json",
+  //         "Content-Type": "application/json"
+  //       },
+  //       body: JSON.stringify({
+  //         accounts: [signerAddr],
+  //         amount: Number(hre.ethers.utils.parseEther("1").toString())
+  //       })
+  //     }
+  //   );
+  // }
 
   const Factory = await hre.ethers.getContractFactory("Factory");
   const factory = await Factory.deploy(GOVERNANCE_ADDRESS);
@@ -27,7 +52,13 @@ async function main() {
 
   console.log("Router deployed to:", router.address);
 
-  tx = await factory.createVault(POOL_ADDRESS, STRATEGY_MANAGER_ADDRESS, 5000, 7000, 0);
+  tx = await factory.createVault(
+    POOL_ADDRESS,
+    STRATEGY_MANAGER_ADDRESS,
+    100000,
+    0,
+    0
+  );
   await tx.wait();
 
   const vaultAddress = await factory.managerVault(STRATEGY_MANAGER_ADDRESS);
@@ -36,24 +67,21 @@ async function main() {
 
   if (etherscan_verify) {
     await hre.run("verify:verify", {
-        address: factory.address,
-        constructorArguments: [GOVERNANCE_ADDRESS],
+      address: factory.address,
+      constructorArguments: [GOVERNANCE_ADDRESS]
     });
 
     await hre.run("verify:verify", {
-        address: router.address,
-        constructorArguments: [factory.address],
+      address: router.address,
+      constructorArguments: [factory.address]
     });
 
     await hre.run("verify:verify", {
-        address: vaultAddress,
-        constructorArguments: [POOL_ADDRESS, 5000, 7000, 0],
+      address: vaultAddress,
+      constructorArguments: [POOL_ADDRESS, 100000, 0, 0]
     });
-
   }
-
 }
-
 
 main()
   .then(() => process.exit(0))
